11ccaf0edb868fcc5dff898ffc35da8d41d5d426
shehzar-abbasi-SG <shehzer@stay.gold>
2025-01-08 18:12:10 -0500

    static pages

--- src/app/(routes)/single-page-form/page.tsx
+++ src/app/(routes)/single-page-form/page.tsx
@@ -1,30 +1,30 @@
 import Form from '@/components/Form/form';
 import React from 'react'
 
-// async function getMediaFormVariants(){
-//     try {
+async function getMediaFormVariants(){
+    try {
     
-//         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/media-submission`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/media-submission`);
     
-//         if (!response.ok) {
-//           throw new Error('Failed to fetch media form data');
-//         }
+        if (!response.ok) {
+          throw new Error('Failed to fetch media form data');
+        }
     
-//         const data = await response.json();
+        const data = await response.json();
   
-//         return data;
-//       } catch (error) {
-//         console.error('Error:', error);
-//         throw error;
-//       }
+        return data;
+      } catch (error) {
+        console.error('Error:', error);
+        throw error;
+      }
     
-// }
+}
 export default async function SubmitMedia() {
-    // const data = await getMediaFormVariants()
-    // console.log('data ==>> ', data);
+    const data = await getMediaFormVariants()
+    console.log('data ==>> ', data);
   return (
     <div>
-    {/* <h1>{data.title}</h1> */}
+    <h1>{data.title}</h1>
     <Form/>
   </div>
   )
