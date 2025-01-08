f3b19db9b13c3d116cca14b9a11db5f9ebcbcdb2
shehzar-abbasi-SG <shehzer@stay.gold>
2025-01-08 17:09:41 -0500

    changes types

--- src/app/(routes)/single-page-form/page.tsx
+++ src/app/(routes)/single-page-form/page.tsx
@@ -1,45 +1,30 @@
 import Form from '@/components/Form/form';
 import React from 'react'
 
-// interface Variant {
-//     id: string;
-//     title: string;
-//     price: { amount: string };
-//   }
+// async function getMediaFormVariants(){
+//     try {
+    
+//         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/media-submission`);
+    
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch media form data');
+//         }
+    
+//         const data = await response.json();
   
-// interface MediaSubmissionProps {
-//       id:string
-//       title: string;
-//       descriptionHtml: string;
-//       variants: {
-//         edges: { node: Variant }[];
-//       };
-//     };
-
-async function getMediaFormVariants(){
-    try {
+//         return data;
+//       } catch (error) {
+//         console.error('Error:', error);
+//         throw error;
+//       }
     
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/media-submission`);
-    
-        if (!response.ok) {
-          throw new Error('Failed to fetch media form data');
-        }
-    
-        const data = await response.json();
-  
-        return data;
-      } catch (error) {
-        console.error('Error:', error);
-        throw error;
-      }
-    
-}
+// }
 export default async function SubmitMedia() {
-    const data = await getMediaFormVariants()
-    console.log('data ==>> ', data);
+    // const data = await getMediaFormVariants()
+    // console.log('data ==>> ', data);
   return (
     <div>
-    <h1>{data.title}</h1>
+    {/* <h1>{data.title}</h1> */}
     <Form/>
   </div>
   )
--- src/app/api/products/[handle]/route.ts
+++ src/app/api/products/[handle]/route.ts
@@ -1,51 +1,50 @@
 import { SHOPIFY_ACCESS_TOKEN, SHOPIFY_API_URL } from "@/config";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { handle: string } }) {
-  const {handle} = params;
-  console.log('productHandle :>> ', handle);
+export async function GET(req: Request, { params }: { params: Promise<{ handle: string }> }) {
+  const {handle} = await  params;
   const query = `
- query GetProductByHandle($handle: String!) {
-  product(handle: $handle) {
-    id
-    title
-    descriptionHtml
-    images(first: 5) {
-      edges {
-        node {
-          src
-          altText
-        }
-      }
-    }
-    variants(first: 5) {
-      edges {
-        node {
+      query GetProductByHandle($handle: String!) {
+        product(handle: $handle) {
           id
           title
-          priceV2 {
-            amount
-            currencyCode
+          descriptionHtml
+          images(first: 5) {
+            edges {
+              node {
+                src
+                altText
+              }
+            }
           }
-          selectedOptions {
-            name
-            value
+          variants(first: 5) {
+            edges {
+              node {
+                id
+                title
+                priceV2 {
+                  amount
+                  currencyCode
+                }
+                selectedOptions {
+                  name
+                  value
+                }
+              }
+            }
+          }
+        
+          productType
+          vendor
+          collections(first: 5) {
+            edges {
+              node {
+                title
+              }
+            }
           }
         }
       }
-    }
-   
-    productType
-    vendor
-    collections(first: 5) {
-      edges {
-        node {
-          title
-        }
-      }
-    }
-  }
-}
   `;
 
   const variables = { handle: handle };
